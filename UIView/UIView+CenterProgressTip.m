07bd1637a6a9d190bb141d143c34ca54abe1181f
7owen <lgw52@hotmail.com>
2016-12-05 19:09:09 +0800

    remove masonry.

--- UIView/UIView+CenterProgressTip.m
+++ UIView/UIView+CenterProgressTip.m
@@ -8,10 +8,12 @@
 
 #import "UIView+CenterProgressTip.h"
 #import <objc/runtime.h>
-#import "Masonry.h"
 
 static void *UIViewCenterProgressLabelKey = &UIViewCenterProgressLabelKey;
 static void *UIViewCenterProgressIndicatorKey = &UIViewCenterProgressIndicatorKey;
+
+static void *UIViewCenterLeftSpaceKey = &UIViewCenterLeftSpaceKey;
+static void *UIViewCenterRightSpaceKey = &UIViewCenterRightSpaceKey;
 
 @implementation UIView (CenterProgressTip)
 
@@ -28,26 +30,44 @@ static void *UIViewCenterProgressIndicatorKey = &UIViewCenterProgressIndicatorKe
 }
 
 - (void)showCenterLoadingTipWithStatus:(NSString*)status afterDelay:(NSTimeInterval)delay {
+    NSMutableArray *constraints = [NSMutableArray arrayWithCapacity:8];
+    
     UILabel *label = (UILabel*)objc_getAssociatedObject(self, UIViewCenterProgressLabelKey);
     UIActivityIndicatorView *indicatorView = (UIActivityIndicatorView*)objc_getAssociatedObject(self, UIViewCenterProgressIndicatorKey);
+    UIView *leftSpaceView = (UIView*)objc_getAssociatedObject(self, UIViewCenterLeftSpaceKey);
+    UIView *rightSpaceView = (UIView*)objc_getAssociatedObject(self, UIViewCenterRightSpaceKey);
+    
+    if (!leftSpaceView) {
+        leftSpaceView = [UIView new];
+        leftSpaceView.translatesAutoresizingMaskIntoConstraints = NO;
+        [self addSubview:leftSpaceView];
+        [constraints addObject:[NSLayoutConstraint constraintWithItem:leftSpaceView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0]];
+    }
+    
+    if (!rightSpaceView) {
+        rightSpaceView = [UIView new];
+        rightSpaceView.translatesAutoresizingMaskIntoConstraints = NO;
+        [self addSubview:rightSpaceView];
+        [constraints addObjectsFromArray:@[[NSLayoutConstraint constraintWithItem:rightSpaceView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeRight multiplier:1.0 constant:0], [NSLayoutConstraint constraintWithItem:leftSpaceView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:rightSpaceView attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0]]];
+    }
+    
     if (!label) {
         label = [UILabel new];
         objc_setAssociatedObject(self, UIViewCenterProgressLabelKey, label, OBJC_ASSOCIATION_RETAIN);
         [self addSubview:label];
-        [label mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.centerY.equalTo(self);
-            make.centerX.equalTo(self).offset(12.f);
-        }];
+        label.translatesAutoresizingMaskIntoConstraints = NO;
+        [constraints addObjectsFromArray:@[[NSLayoutConstraint constraintWithItem:label attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0.f], [NSLayoutConstraint constraintWithItem:label attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:rightSpaceView attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0.f]]];
     }
+    
     if (!indicatorView) {
         indicatorView = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
         objc_setAssociatedObject(self, UIViewCenterProgressIndicatorKey, indicatorView, OBJC_ASSOCIATION_RETAIN);
         [self addSubview:indicatorView];
-        [indicatorView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.centerY.equalTo(self.mas_centerY);
-            make.right.equalTo(label.mas_left).offset(-4.f);
-        }];
+        indicatorView.translatesAutoresizingMaskIntoConstraints = NO;
+        [constraints addObjectsFromArray:@[[NSLayoutConstraint constraintWithItem:indicatorView attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0.f], [NSLayoutConstraint constraintWithItem:indicatorView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:label attribute:NSLayoutAttributeLeft multiplier:1.0 constant:-6], [NSLayoutConstraint constraintWithItem:indicatorView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:leftSpaceView attribute:NSLayoutAttributeRight multiplier:1.0 constant:0]]];
     }
+    [self addConstraints:constraints];
+    
     
     label.text = status;
     [indicatorView startAnimating];
